b2fb806157d230ed54aed14d69f3012206b2189d
MatteoZattini <matteozattini@hotmail.com>
2024-05-27 19:17:05 +0200

    bonus 1 e bonus 2

--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -20,7 +20,10 @@ export default {
   },
 
   mounted() {
-    
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(function(result) {
+        console.log(result)
+        store.carte = result.data.data;
+    })
   }
 }
 </script>
