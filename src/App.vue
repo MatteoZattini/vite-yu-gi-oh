faf4cec83d8afdb7935604d2f65d4433d6586c0b
MatteoZattini <matteozattini@hotmail.com>
2024-05-27 16:59:06 +0200

    m1 ciclo e stampo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script>
+import AppCardList from './components/AppCardList.vue';
+
+export default {
+  components: {
+    AppCardList
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  }
+}
 </script>
 
 <template>
-
+  <h1>ciao</h1>
+<AppCardList />
 </template>
 
 <style>
+
 </style>
---
+++ src/components/AppSingleCard.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name: "AppSingleCard",
+
+    props: {
+        img: String,
+        title: String,
+        archetype: String
+    },
+
+  data() {
+    return {
+
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+    
+  }
+}
+</script>
+
+<template>
+<div>
+    <div>
+        <img :src="img" alt="">
+    </div>
+    <div>
+        <h3>{{ title }}</h3>
+    </div>
+    <div>
+        <span>{{ archetype }}</span>
+    </div>
+</div>
+</template>
+
+<style>
+</style>
